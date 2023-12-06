a0703a4ddcd4135cc30ac46eef46560006cbbff7
Fabrice Magnan <magnan.fabrice@gmail.com>
2023-12-06 23:10:22 +0100

    correction parametre pour build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/Kasa/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/Kasa/Accueil" : "/Accueil",
   server: {
     historyApiFallback: true, // Activez cette option
   },
